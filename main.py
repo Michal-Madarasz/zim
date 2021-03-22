5512a6a1b6c21bbc42deddd22919981920e3cc4d
Michał Madarasz <238903@student.pwr.edu.pl>
2021-03-22 19:37:21 +0100

    Operowanie z danymi
    - Dodanie nazw dla kolumn w zbiorze dannych
    - Próba użycia algorytmu selekcji cech

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
-from sklearn.neighbors import KNeighborsClassifier
 import pandas as pd
+import numpy as np
+from sklearn.feature_selection import SelectKBest, f_classif
 
-data = pd.read_csv("./data/prepared_data/diagnosis.csv", encoding='UTF-16')
+if __name__ == '__main__':
+    data = pd.read_csv("./data/prepared_data/diagnosis.csv", encoding='UTF-16')
+    print(data)
 
-print(data)
+    predictors = ["Temperature", "Nausea", "LumberPain", "ConUrine",
+                  "MictPains", "UrethraBurn"]
+
+    selector = SelectKBest(f_classif, k=5)
+    # TODO How to handle text data in scikit-learn
+    # selector.fit(data[predictors], data["Inflammation"])
+    # scores = -np.log10(selector.pvalues_)
