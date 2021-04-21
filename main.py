3e319ff91a714bea2c2910c51255e5582306a44a
Kamil Kucharski <k.kucharski@itl.waw.pl>
2021-04-21 19:15:49 +0200

    Implementacja środowiska początek

--- main.py
+++ main.py
@@ -2,26 +2,31 @@ import pandas as pd
 import numpy as np
 from sklearn.feature_selection import SelectKBest, f_classif
 import matplotlib.pyplot as plt
+from sklearn.model_selection import RepeatedKFold, train_test_split
 
-from names_dict import FEATURES
+from names_dict import COLUMNS
 
 if __name__ == '__main__':
     data = pd.read_csv("./data/prepared_data/diagnosis.csv", encoding='UTF-16')
 
-    predictors = dict(FEATURES[1:])
+    predictors = dict(COLUMNS[1:])
 
     selector = SelectKBest(f_classif, k=3)
     for i in predictors:
         data[i] = data[i].map({'yes': 1, 'no': 0})
 
-    selector.fit_transform(data[predictors], data["Inflammation"])
-    scores = -np.log10(selector.pvalues_)
+    print(data)
 
+    num_of_neighbours = [1, 5, 10]
+    type_of_metric = ["euclidean", "manhattan"]
 
-    scores.sort()
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y,
+        test_size=.30,
+        random_state=1234
+    )
 
-    plt.figure(figsize=(12,6))
-    plt.axes([0.45, 0.35, 0.5, 0.5])
-    plt.barh(range(len(predictors)), scores)
-    plt.yticks(range(len(predictors)), predictors.values())
-    plt.show()
+    rkf = RepeatedKFold(n_splits=2, n_repeats=5, random_state=1234)
+    print(rkf)
+    scores = []
+
--- names_dict.py
+++ names_dict.py
@@ -6,14 +6,4 @@ COLUMNS = (
            ("MictPains", "Bóle mikcji"),
            ("UrethraBurn", "Pieczenie cewki moczowej, swędzenie, obrzęk ujścia cewki moczowej"),
            ("Inflammation", "Zapalenie pęcherza moczowego"),
-           ("Nephritis", "Zapalenie nerek pochodzenia miedniczkowego"),
-)
-
-FEATURES = (
-    ("Temperature", "Temperatura"),
-    ("Nausea", "występowanie nudności"),
-    ("LumberPain", "Ból lędźwiowy"),
-    ("ConUrine", "Parcie moczu (ciągła potrzeba oddawania moczu)"),
-    ("MictPains", "Bóle mikcji"),
-    ("UrethraBurn", "Pieczenie cewki moczowej, swędzenie, obrzęk ujścia cewki moczowej"),
 )
