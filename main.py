44a0780478b7cec644540a534b9f5ef208346009
Kamil Kucharski <k.kucharski@itl.waw.pl>
2021-04-22 12:20:58 +0200

    Poprawa wyglądu tabelki statystyki

--- main.py
+++ main.py
@@ -63,8 +63,15 @@ if __name__ == '__main__':
         for j in range(stats_array_len):
             t_statistic[i, j], p_value[i, j] = ttest_rel(final_scores[i], final_scores[j], nan_policy='omit')
 
-    print("t_statistic:\n", t_statistic)
-    print("p_value:\n", p_value)
+    headers = ['1n_eu','5n_eu','10n_eu','1n_man','5n_man','10n_man']
+    names_column = np.array([['1n_eu'],['5n_eu'],['10n_eu'],['1n_man'],['5n_man'],['10n_man']])
+    t_statistic_table = np.concatenate((names_column, t_statistic), axis=1)
+    t_statistic_table = tabulate(t_statistic_table, headers, floatfmt=".2f")
+    p_value_table = np.concatenate((names_column, p_value), axis=1)
+    p_value_table = tabulate(p_value_table, headers, floatfmt=".2f")
+
+    print("t_statistic:\n", t_statistic_table)
+    print("p_value:\n", p_value_table)
 
     headers = ['1n_eu','5n_eu','10n_eu','1n_man','5n_man','10n_man']
     names_column = np.array([['1n_eu'],['5n_eu'],['10n_eu'],['1n_man'],['5n_man'],['10n_man']])
@@ -72,5 +79,5 @@ if __name__ == '__main__':
     advantage = np.zeros((stats_array_len, stats_array_len))
     advantage[t_statistic > 0] = 1
     advantage_table = tabulate(np.concatenate(
-        (names_column, advantage), axis=1), headers)
+        (names_column, advantage), axis=1), headers, floatfmt=".2f")
     print("Advantage:\n", advantage_table)
